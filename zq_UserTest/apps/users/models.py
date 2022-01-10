8dc87ac2b36017e6ba24f1db452f9a98d4b4a84b
NagisaCo <yjr888@vip.qq.com>
2022-01-10 22:50:26 +0800

    feat(users): 新增用户模型

--- zq_UserTest/apps/users/models.py
+++ zq_UserTest/apps/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from zq_UserTest.utils.storages import AvatarStorage
+
+
+class User(AbstractUser):
+    """
+    用户信息，扩展相关字段
+    """
+
+    avatar = models.ImageField(upload_to='avatar', default='avatar/default.jpg', verbose_name='头像',
+                               storage=AvatarStorage)
+    mobile = models.CharField(max_length=11, unique=True, verbose_name='手机号')
+    nickname = models.CharField(max_length=20, unique=True, verbose_name='昵称')
+    description = models.CharField(max_length=200, verbose_name='个人简介')
+
+    class Meta:
+        app_label = 'users'
+        db_table = 'tb_user'
+        verbose_name = '用户信息'
+        verbose_name_plural = verbose_name
